3a5e283d8ce7dd52e6c0b7ce741f4c3c10424d01
AntonyChen <antony.global@icloud.com>
2025-02-12 23:52:40 +0800

    Add ant-design

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,26 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import styles from './App.module.css'
+import {Layout, Typography, Input} from 'antd'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
+        <Layout.Header>
+          <img src={viteLogo} alt=''/>
+          <Typography.Title level={3}>React 旅游网</Typography.Title>
+          <Input.Search placeholder={'请输入旅游目的地，主题或关键字'} />
+        </Layout.Header>
+      </div>
+
+
+
+
+
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -28,7 +41,7 @@ function App() {
       </div>
       <p className={styles['read-the-docs']}>
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
