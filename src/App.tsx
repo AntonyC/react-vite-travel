e8cfc6c12fed250464e0b169017562a5ea816542
AntonyChen <antony.global@icloud.com>
2025-02-13 23:50:22 +0800

    formatted code

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import styles from './App.module.css'
-import {Layout, Typography, Input} from 'antd'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import styles from './App.module.css';
+import { Layout, Typography, Input } from 'antd';
 
 function App() {
-  // const [count, setCount] = useState(0)
+	// const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <Layout.Header>
-          <img src={viteLogo} alt=''/>
-          <Typography.Title level={3}>React 旅游网</Typography.Title>
-          <Input.Search placeholder={'请输入旅游目的地，主题或关键字'} />
-        </Layout.Header>
-      </div>
+	return (
+		<>
+			<div>
+				<Layout.Header>
+					<img src={viteLogo} alt='' />
+					<Typography.Title level={3}>React 旅游网</Typography.Title>
+					<Input.Search placeholder={'请输入旅游目的地，主题或关键字'} />
+				</Layout.Header>
+			</div>
 
-
-
-
-
-      {/* <div>
+			{/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -42,8 +38,8 @@ function App() {
       <p className={styles['read-the-docs']}>
         Click on the Vite and React logos to learn more
       </p> */}
-    </>
-  )
+		</>
+	);
 }
 
-export default App
+export default App;
