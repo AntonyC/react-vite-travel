509855ef906fc86e79a3475fdaa3f34d94439f58
AntonyChen <antony.global@icloud.com>
2025-02-22 18:15:08 +0800

    refactor

--- src/pages/detail/DetailPage.tsx
+++ src/pages/detail/DetailPage.tsx
@@ -1,15 +1,10 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React from 'react';
+import { useParams } from 'react-router-dom';
 
 type MatchParams = {
 	touristRouteId: string;
 	other: string;
 };
-
-interface MatchParams2 {
-	touristRouteId: string;
-	other: string;
-}
 
 // type str = "str";
 // interface str2 = "str";
@@ -17,7 +12,7 @@ interface MatchParams2 {
 export const DetailPage: React.FC = () => {
 	// var params = useParams<"touristRouteId">();
 	// var params = useParams<MatchParams>();
-	var params = useParams<keyof MatchParams2>();
+	var params = useParams<keyof MatchParams>();
 	return (
 		<h1>
 			旅游路线详情页, 路线id: {params.touristRouteId} {params.other}
