13385c6ae37dbf1ba9a51af443ad9b323901a6c5
AntonyChen <antony.global@icloud.com>
2025-02-23 00:23:01 +0800

    Add product detail page

---
+++ src/redux/productDetail/slice.ts
@@ -0,0 +1,55 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { productMockData } from './mockup';
+
+interface ProductDetailState {
+	loading: boolean;
+	error: string | null;
+	data: any;
+}
+
+const initialState: ProductDetailState = {
+	loading: true,
+	error: null,
+	data: null,
+};
+
+export const getProductDetail = createAsyncThunk(
+	'productDetail/getProductDetail',
+	// async (touristRouteId: string, thunkAPI) => {
+	//   const { data } = await axios.get(
+	//     `http://localhost:8080/api/touristRoutes/${touristRouteId}`
+	//   );
+	//   return data;
+	// }
+	async (touristRouteId: string, thunkAPI) => {
+		const data = await new Promise(resolve => {
+			setTimeout(() => {
+				productMockData.description = `${productMockData.description}-${touristRouteId}`;
+				resolve(productMockData);
+			}, 1000);
+		});
+
+		return data;
+	}
+);
+
+export const productDetailSlice = createSlice({
+	name: 'productDetail',
+	initialState,
+	reducers: {},
+	extraReducers: builder => {
+		builder
+			.addCase(getProductDetail.pending, state => {
+				state.loading = true;
+			})
+			.addCase(getProductDetail.fulfilled, (state, action) => {
+				state.data = action.payload;
+				state.loading = false;
+				state.error = null;
+			})
+			.addCase(getProductDetail.rejected, (state, action) => {
+				state.loading = false;
+				state.error = action.payload as string | null;
+			});
+	},
+});
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,21 @@
-import { createStore } from 'redux';
-import languageReducer from "./language/languageReducer";
+import languageReducer from './language/languageReducer';
+import { actionLog } from './middlewares/actionLog';
+import { productDetailSlice } from './productDetail/slice';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
-const store = createStore(languageReducer);
+const rootReducer = combineReducers({
+	language: languageReducer,
+	productDetail: productDetailSlice.reducer,
+});
 
-export type RootState = ReturnType<typeof store.getState>
+// const store = createStore(rootReducer, applyMiddleware(thunk, actionLog));
+const store = configureStore({
+	reducer: rootReducer,
+	middleware: getDefaultMiddleware => getDefaultMiddleware().concat(actionLog),
+	devTools: true,
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 
 export default store;
