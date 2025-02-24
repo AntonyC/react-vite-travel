ff34c311bb6c760a32d3fdef06711d722a2783ec
AntonyChen <antony.global@icloud.com>
2025-02-24 22:27:37 +0800

    add code

--- src/redux/user/mockup.ts
+++ src/redux/user/mockup.ts
@@ -1,3 +1,4 @@
 export const tokenMockData = {
-  token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFudG9ueSJ9._lR53QxHU6gzL5S_OtnwwZKT2UsbHa8FVAUMcoLqyDI'
+	token:
+		'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkFudG9ueSJ9.D0PtAIXO9eQReUt0YH8MPNuyeXjXg8l0VHY83hASc5Q',
 };
