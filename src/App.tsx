6e2033e3b496adcc71ec200bd6fba041a335b9de
AntonyChen <antony.global@icloud.com>
2025-02-27 00:13:38 +0800

    Add private route

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,14 @@ import {
 	DetailPage,
 	SearchPage,
 } from './pages';
+import { Navigate } from 'react-router-dom';
+import { useSelector, useAppDispatch } from './redux/hooks';
+import { getShoppingCart } from './redux/shoppingCart/slice';
+
+const PrivateRoute = ({ children }) => {
+	const jwt = useSelector(s => s.user.token);
+	return jwt ? children : <Navigate to='/signIn' />;
+};
 
 function App() {
 	return (
@@ -19,6 +27,22 @@ function App() {
 					<Route path='/register' element={<RegisterPage />} />
 					<Route path='/detail/:touristRouteId' element={<DetailPage />} />
 					<Route path='/search/:keywords' element={<SearchPage />} />
+					<Route
+						path='/shoppingCart'
+						element={
+							<PrivateRoute>
+								<ShoppingCartPage />
+							</PrivateRoute>
+						}
+					/>
+					<Route
+						path='/placeOrder'
+						element={
+							<PrivateRoute>
+								<PlaceOrderPage />
+							</PrivateRoute>
+						}
+					/>
 					<Route path='*' element={<h1>404 not found 页面去火星了</h1>} />
 				</Routes>
 			</BrowserRouter>
